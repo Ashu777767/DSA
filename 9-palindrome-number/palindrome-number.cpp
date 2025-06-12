bee648c7ef5fe71a8a7f105df9c580fdf278c8f3
ashutosh kuamr jha <149995452+Ashu777767@users.noreply.github.com>
2025-06-12 19:31:54 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (68.88%) - LeetSync

--- 9-palindrome-number/palindrome-number.cpp
+++ 9-palindrome-number/palindrome-number.cpp
@@ -1,15 +1,17 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-      if (x < 0 || (x % 10 == 0 && x != 0)) return false;
-
-        int reversedHalf = 0;
-        
-        while (x > reversedHalf) {
-            reversedHalf = reversedHalf * 10 + x % 10;
-            x /= 10;
-        }
-
-        return (x == reversedHalf || x == reversedHalf / 10);
+        long val = 0;
+        int original = x;
+        while(x>0)
+        {
+          int digit = x%10;
+         val = val*10+digit ;
+         x/=10;
+         }
+         if(val == original)
+         return true;
+     return false;
     }
+    
 };
