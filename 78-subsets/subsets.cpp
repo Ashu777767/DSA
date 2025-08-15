a0d802a1252a4ea6231df225d42bf9447a7d62dc
ashutosh kumar jha <149995452+Ashu777767@users.noreply.github.com>
2025-08-15 17:28:57 +0530

    Time: 3 ms (31.94%) | Memory: 16.9 MB (6.40%) - LeetSync

---
+++ 78-subsets/subsets.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+void solveans(vector<int> nums, vector<vector<int>> &ans,int index,vector<int>output){
+    if(index>=nums.size()){
+        ans.push_back(output);
+        return ;
+    }
+    //exclude
+    solveans(nums,ans,index+1,output);
+    //include
+    output.push_back(nums[index]);
+    solveans(nums,ans,index+1,output);
+
+}
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>>ans;//powerset
+        vector<int>output;
+        int index = 0;
+        solveans(nums,ans,index,output);
+     return ans;
+    }
+};
