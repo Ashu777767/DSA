372cb3435eeabda203490a266f96dbe9464334c3
ashutosh kumar jha <149995452+Ashu777767@users.noreply.github.com>
2025-08-20 20:31:04 +0530

    Time: 0 ms (100.00%) | Memory: 27.1 MB (79.85%) - LeetSync

--- 344-reverse-string/reverse-string.cpp
+++ 344-reverse-string/reverse-string.cpp
@@ -1,10 +1,16 @@
 class Solution {
 public:
+void helper(vector<char>& s, int left, int right) {
+        // base case: when pointers cross
+        if (left >= right) return;
+
+        // swap current characters
+        swap(s[left], s[right]);
+
+        // recursive call for inner substring
+        helper(s, left + 1, right - 1);
+    }
     void reverseString(vector<char>& s) {
-        int left = 0;
-        int right = s.size()-1;
-        while(left<=right){
-            swap(s[left++],s[right--]);
-        }
+         helper(s, 0, s.size() - 1);
     }
 };
