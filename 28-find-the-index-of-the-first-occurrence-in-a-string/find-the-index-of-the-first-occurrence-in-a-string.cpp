c3c4cbff2a5cef60fb75e915692829926512e96e
ashutosh kumar jha <149995452+Ashu777767@users.noreply.github.com>
2025-08-21 20:52:37 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (5.29%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,6 +1,48 @@
 class Solution {
 public:
-    int strStr(string haystack, string needle) {
-        return haystack.find(needle);
+ vector<int> buildLPS(string &needle) {
+        int m = needle.size();
+        vector<int> lps(m, 0);
+        int len = 0; // length of previous longest prefix suffix
+        int i = 1;
+
+        while (i < m) {
+            if (needle[i] == needle[len]) {
+                len++;
+                lps[i] = len;
+                i++;
+            } else {
+                if (len != 0) {
+                    len = lps[len - 1];
+                } else {
+                    lps[i] = 0;
+                    i++;
+                }
+            }
+        }
+        return lps;
     }
+    int strStr(string haystack, string needle) {
+        int n = haystack.size();
+        int m = needle.size();
+        if (m == 0) return 0;
+
+        vector<int> lps = buildLPS(needle);
+
+        int i = 0, j = 0;
+        while (i < n) {
+            if (haystack[i] == needle[j]) {
+                i++; j++;
+                if (j == m) return i - m; // found
+            } else {
+                if (j != 0) {
+                    j = lps[j - 1];
+                } else {
+                    i++;
+                }
+            }
+        }
+        return -1;
+    }
+    
 };
