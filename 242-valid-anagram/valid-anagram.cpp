3c9c6870b5a46053ff06a995204e7e3de57e9004
ashutosh kumar jha <149995452+Ashu777767@users.noreply.github.com>
2025-08-21 21:53:52 +0530

    Time: 3 ms (51.89%) | Memory: 10 MB (11.62%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,16 +1,19 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
+        if(s.size()!= t.size()){    //edge case
+        return false;             
+        }
         unordered_map<char,int>str1;
-        unordered_map<char,int>str2;
         for(char ch:s){
             str1[ch]++;
         }
         for(char ch:t){
-            str2[ch]++;
+            if(--str1[ch]<0)
+            return false;
         }
 
-       return str1 == str2;
+       return true;
     }
     
 };
