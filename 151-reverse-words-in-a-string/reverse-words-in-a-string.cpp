383a07579f3dfe7c8aab08dce9ec2af71ddc5aef
ashutosh kumar jha <149995452+Ashu777767@users.noreply.github.com>
2025-08-21 23:56:28 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (98.48%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -1,51 +1,37 @@
 class Solution {
 public:
-string trim(string &s) {
-        int start = s.find_first_not_of(' ');
-        if (start == string::npos) {
-            return "";
-        }
-
-        int end = s.find_last_not_of(' ');
-        return s.substr(start, end - start + 1);
-    }
-void reverseword(string &s,int first ,int end){
-    while(first<end){
-        swap(s[first++],s[end--]);
-    }
-}
-void stringreverse(string &s,int start,int e){
-   while(start<e){
-    swap(s[start++],s[e--]);
-   }
-}
     string reverseWords(string s) {
-        s = trim(s);
-        int i = 0,j = 0;
-      int n = s.size();
-      while(j<n){
-      while(j<n && s[j] == ' '){
-        j++;
-      }
-        if(j<n){
-            if(i!= 0)
-            s[i++] = ' ';
-            while(j<n && s[j]!= ' '){
-                s[i++] = s[j++];
-            }
-        }
-      }
-       s.resize(i);
-        n = s.size();
-        stringreverse(s,0,n-1);
-        int first = 0;
+        int i = 0, j = 0;
+   while (i < s.size()) {
+    // skip spaces
+    while (i < s.size() && s[i] == ' ') i++;
 
-        for(int end = 0;end<=n;end++){
-            if(s[end] == ' '|| end == n){
-                reverseword(s,first,end-1);
-                first = end+1;
+    // if not the first word, add one space before word
+    if (j > 0 && i < s.size()) {
+        s[j++] = ' ';
+    }
+
+    // copy characters of the word
+    while (i < s.size() && s[i] != ' ') {
+        s[j++] = s[i++];
+    }
+}
+s.resize(j);
+
+        reverse(s.begin(),s.end());
+        int stw = 0;
+        for(int enw = 0;enw<s.size();enw++){
+            if(!isalnum(s[enw])){
+            reverse(s.begin()+stw,s.begin()+enw);
+            stw = enw+1;
             }
+            
         }
-    return s;
+        int last = s.size() -1;
+        while(stw<last){
+           swap(s[stw++],s[last--]);
+        }
+
+        return s;
     }
 };
