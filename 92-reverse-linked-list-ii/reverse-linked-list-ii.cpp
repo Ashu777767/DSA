cf61e7cdaaecd5a25863c087ffa930d9bf5dfe9f
ashutosh kumar jha <149995452+Ashu777767@users.noreply.github.com>
2025-08-30 15:02:53 +0530

    Time: 0 ms (100.00%) | Memory: 11.1 MB (72.47%) - LeetSync

---
+++ 92-reverse-linked-list-ii/reverse-linked-list-ii.cpp
@@ -0,0 +1,42 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseBetween(ListNode* head, int left, int right) {
+        //creates a dummy
+        ListNode* dummy = new ListNode(0,head); //next = head val = 0
+        //marks the previous and starting node to reverse
+        ListNode* leftpre = dummy;  //if left = 1 then we need a prev node so we use this
+        ListNode* curr = head;
+        for(int i = 0;i<left-1;i++){
+            leftpre = leftpre->next;
+            curr = curr->next;
+        }
+        ListNode*startMergeNode = curr;
+        //reverseing the given part
+        ListNode* prev = NULL;
+        ListNode* Next = NULL;
+        for(int i = 0;i<(right-left+1);i++){
+            Next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = Next;
+        }
+        //now linking the starting and ending to reversed list
+        //links 1->4
+        leftpre->next = prev;
+        //links the 2 ->5
+        startMergeNode->next = curr;
+        
+        //return the list 
+        return dummy->next;
+    }
+};
