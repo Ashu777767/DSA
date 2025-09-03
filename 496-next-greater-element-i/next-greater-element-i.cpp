6582f23d5d93e475a00731452f54acf457614f31
ashutosh kumar jha <149995452+Ashu777767@users.noreply.github.com>
2025-09-03 18:10:35 +0530

    Time: 0 ms (100.00%) | Memory: 13 MB (15.47%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        unordered_map<int,int>map;//to store nums2 element and next greater element pair
         stack<int>s;
-        for(int i = (nums2.size()-1) ;i>=0;i--){
-             while(s.size()>0 && s.top()<=nums2[i]){   //right element exist karta hai lekin greater nhi hai toh pop
+        unordered_map<int,int>maps;
+        for(int i = nums2.size()-1;i>=0;i--){
+            while(!s.empty() && s.top()<nums2[i]){
                 s.pop();
-             }
-             if(s.empty()){   //matlab mila hi nahi greater element right side
-                 map[nums2[i]] = -1;
-             }
-             else{   //mil gaya greater element
-                 map[nums2[i]] = s.top();
-             }
-             s.push(nums2[i]);
+            }
+            if(s.empty()){
+                maps[nums2[i]] = -1;
+            }
+            else{
+                maps[nums2[i]] = s.top();
+            }
+            s.push(nums2[i]);
         }
-         vector<int>ans;       //
+        vector<int>ans;
         for(int val:nums1){
-             ans.push_back(map[val]);
+            ans.push_back(maps[val]);
         }
         return ans;
     }
