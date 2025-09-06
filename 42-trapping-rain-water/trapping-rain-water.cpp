059276a3fd1a8f1f852bdbbf570594d98492c729
ashutosh kumar jha <149995452+Ashu777767@users.noreply.github.com>
2025-09-06 07:48:44 +0530

    Time: 0 ms (100.00%) | Memory: 27.1 MB (39.98%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -1,23 +1,20 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
+        int n = height.size();
+        vector<int>leftborder(n,0);
+        vector<int>rightborder(n,0);
+        leftborder[0] = height[0];
+        rightborder[n-1] = height[n-1];
+        for(int i = 1;i<n;i++){
+            leftborder[i] = max(leftborder[i-1],height[i]);
+        }
+        for(int i = n-2;i>=0;i--){
+            rightborder[i] = max(rightborder[i+1],height[i]);
+        }
         int ans = 0;
-        int left = 0;
-        int right = height.size()-1;
-        int leftmax = INT_MIN;
-        int rightmax = INT_MIN;
-        while(left<right){
-             leftmax = max(leftmax,height[left]);     //max left border as per current bar
-             rightmax = max(rightmax,height[right]);   //max right border as per current bar
-             if(leftmax<rightmax){                  //min(leftmax,rightmax)-height[i]  formula
-                ans+=(leftmax-height[left]);
-                left++;
-             }
-             else{
-                ans+=(rightmax-height[right]);
-                right--;
-             }
-
+        for(int i = 0;i<n;i++){
+            ans+= (min(leftborder[i],rightborder[i])-height[i]);
         }
         return ans;
     }
