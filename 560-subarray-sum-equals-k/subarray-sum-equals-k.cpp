29f25f874682bc75fd8395acf2865b689e013c62
ashutosh kumar jha <149995452+Ashu777767@users.noreply.github.com>
2025-09-13 08:06:50 +0530

    Time: 37 ms (82.20%) | Memory: 46.8 MB (36.49%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -2,26 +2,23 @@ class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
         int n = nums.size();
-      vector<int>p(n,0);
-      int count = 0;
-      int val;
-      p[0] = nums[0];
-      for(int i = 1;i<n;i++){
-        p[i] = p[i-1]+nums[i];
-      }
-      unordered_map<int,int>map;
-      for(int j = 0;j<n;j++){
-        if(p[j] == k)
-        count++;
-        val = p[j] - k;
-        if(map.find(val)!=map.end()){
-            count+=map[val];
+        vector<int>prefix(n);
+        int count = 0;
+        prefix[0] = nums[0];
+        for(int i = 1;i<n;i++){
+           prefix[i] = prefix[i-1]+nums[i];
         }
-      if(map.find(p[j]) == map.end()){
-       map[p[j]] = 0;
-      }
-     map[p[j]]++;
-      }
-      return count;
+        unordered_map<int,int>mp;
+        for(int r = 0;r<n;r++){
+            if(prefix[r] == k){
+                count++;
+            }
+            int val = prefix[r] - k;   //to get left part
+            if(mp.find(val) != mp.end()){
+                count+=mp[val];
+            }
+            mp[prefix[r]]++;
+        }
+        return count;
     }
 };
