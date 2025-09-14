338d35ba9c41c5fba24cb4db3fc770dacb104924
ashutosh kumar jha <149995452+Ashu777767@users.noreply.github.com>
2025-09-14 19:06:32 +0530

    Time: 7 ms (33.71%) | Memory: 26.9 MB (16.09%) - LeetSync

---
+++ 680-valid-palindrome-ii/valid-palindrome-ii.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool validPalindrome(string s) {
+        int left = 0, right = s.size() - 1;
+        
+        while (left < right) {
+            if (s[left] == s[right]) {
+                left++;
+                right--;
+            } else {
+                // Check both possibilities: skip left or skip right //this is the right way
+                return isPalindrome(s, left + 1, right) || isPalindrome(s, left, right - 1);
+            }
+        }
+        
+        return true;
+    }
+
+private:
+    bool isPalindrome( string s, int l, int r) {
+        while (l < r) {
+            if (s[l] != s[r]) return false;
+            l++;
+            r--;
+        }
+        return true;
+    }
+};
