4fabd2626803efa06fc22024999116d1c99c099b
ashutosh kumar jha <149995452+Ashu777767@users.noreply.github.com>
2025-09-14 17:16:50 +0530

    Time: 13 ms (79.48%) | Memory: 24.8 MB (85.73%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -2,15 +2,16 @@ class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         unordered_map<string,vector<string>>anagram;
-        for(string s :strs){
-            string key = s;
-            sort(key.begin(),key.end());
-            anagram[key].push_back(s);
-        }
-        vector<vector<string>> answer;
-        for(auto val:anagram){
-            answer.push_back(val.second);
-        }
-        return answer;
+         for(string st : strs){
+            string key = st;
+            sort(key.begin(),key.end());  //creates the key for anagram words
+            anagram[key].push_back(st);   //stores the anagrams  as values of the key which came from its sorted version
+         }
+         vector<vector<string>>answer;
+         for(auto &it:anagram){
+            answer.push_back(it.second);
+         }
+
+         return answer;
     }
 };
