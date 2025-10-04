a8d8436545b2900667c7d06d53490142408724c7
ashutosh kumar jha <149995452+Ashu777767@users.noreply.github.com>
2025-10-04 18:59:06 +0530

    Time: 4 ms (17.27%) | Memory: 17.5 MB (6.12%) - LeetSync

---
+++ 90-subsets-ii/subsets-ii.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+void solveans(vector<int> nums, vector<vector<int>> &ans,int index,vector<int>output){
+    if(index>=nums.size()){
+        ans.push_back(output);
+        return ;
+    }
+
+   
+    //include
+    output.push_back(nums[index]);
+    solveans(nums,ans,index+1,output);
+
+     //exclude
+     output.pop_back();
+     int idx = index+1;
+     while(idx<nums.size() && nums[idx] == nums[idx-1]){
+        idx++;
+     }
+    solveans(nums,ans,idx,output);
+
+}
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        vector<vector<int>>ans;//powerset
+        vector<int>output;
+        int index = 0;
+        solveans(nums,ans,index,output);
+     return ans;
+    }
+};
