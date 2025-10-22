c8f7efd26cc358d970b723f33b1b92c43d90b9d4
ashutosh kumar jha <149995452+Ashu777767@users.noreply.github.com>
2025-10-22 11:07:11 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (94.68%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
-void solveans(vector<int> nums, vector<vector<int>> &ans,int index,vector<int>output){
-    if(index>=nums.size()){
-        ans.push_back(output);
+ void findsubsets(vector<int>& nums, vector<vector<int>>& ans,vector<int>& result,int idx)
+ {
+    if(idx >= nums.size()) //base case
+    {
+        ans.push_back({result});
         return ;
     }
-
-   
     //include
-    output.push_back(nums[index]);
-    solveans(nums,ans,index+1,output);
+    result.push_back(nums[idx]);
+    findsubsets(nums,ans,result,idx+1);
+    //exclude
+    result.pop_back();
+    findsubsets(nums,ans,result,idx+1);
 
-     //exclude
-     output.pop_back();
-    solveans(nums,ans,index+1,output);
+ }
 
-}
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>>ans;//powerset
-        vector<int>output;
-        int index = 0;
-        solveans(nums,ans,index,output);
-     return ans;
+        vector<vector<int>>ans;
+        vector<int>result;
+        int idx = 0;
+        findsubsets(nums,ans,result,idx);
+        return ans;
     }
 };
