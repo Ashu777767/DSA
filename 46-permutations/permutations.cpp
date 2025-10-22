a94c3b154cb86f19443fbc21e6e3eed85e859610
ashutosh kumar jha <149995452+Ashu777767@users.noreply.github.com>
2025-10-22 15:13:15 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (87.63%) - LeetSync

--- 46-permutations/permutations.cpp
+++ 46-permutations/permutations.cpp
@@ -1,22 +1,25 @@
 class Solution {
 public:
-  void  permutations(vector<int>& nums,int idx, vector<vector<int>>& ans)
-  {
-    if(idx>=nums.size())
+void Allpermutations(vector<int>& nums, vector<vector<int>>& ans,int idx)
+{
+    if(idx>=nums.size())   //base case
     {
         ans.push_back({nums});
         return ;
     }
-    for(int i = idx;i<nums.size();i++){
-        swap(nums[i],nums[idx]);
-        permutations(nums,idx+1,ans);
-        swap(nums[i],nums[idx]);   //backtracking to get the previous array
+
+    for(int i = idx;i<nums.size();i++)
+    {
+        swap(nums[idx],nums[i]);
+        Allpermutations(nums,ans,idx+1);
+        swap(nums[idx],nums[i]);  //backtrack
     }
-  }
+}
 
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>>ans;
-        permutations(nums,0,ans);
+        int idx = 0;
+        Allpermutations(nums,ans,idx);
         return ans;
     }
 };
