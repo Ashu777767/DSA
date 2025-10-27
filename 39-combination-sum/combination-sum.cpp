88570c286bc8c5c44648326097fcc099b9cf0958
ashutosh kumar jha <149995452+Ashu777767@users.noreply.github.com>
2025-10-27 21:10:42 +0530

    Time: 12 ms (22.86%) | Memory: 18.5 MB (22.49%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -3,6 +3,11 @@ public:
      set<vector<int>>s;
     void getAllcombinations(vector<int>& arr,int target,int idx,vector<vector<int>>&ans,vector<int>& combi)
     {
+        
+        if(idx == arr.size() ||target<0){  //if all values of canditate are over or target becomes minus then we should stop and return
+            return ;
+        }
+
         if(target == 0){    //solution base case
         if(s.find({combi}) == s.end()){  //using set for only uniquee answers
             ans.push_back({combi});
@@ -10,17 +15,6 @@ public:
         }
            return ;
         }
-        if(idx == arr.size() ||target<0){  //if all values of canditate are over or target becomes minus then we should stop and return
-            return ;
-        }
-
-        // if(target == 0){    //solution base case
-        // if(s.find({combi}) == s.end()){  //using set for only uniquee answers
-        //     ans.push_back({combi});
-        //     s.insert({combi});
-        // }
-        //    return ;
-        // }
          
         combi.push_back(arr[idx]);   //->inclding for both single and multiple inclusion
       //single inclusion
