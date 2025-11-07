ffa8923d2a73c7d899fa1a8ba29d0c9c33400fda
ashutosh kumar jha <149995452+Ashu777767@users.noreply.github.com>
2025-11-07 15:08:07 +0530

    Time: 0 ms (100.00%) | Memory: 27.5 MB (35.46%) - LeetSync

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -11,29 +11,34 @@
  */
 class Solution {
 public:
-  int search(int val,vector<int>& inorder,int left,int right){
-    for(int i = left;i<=right;i++)
-    {
-        if(inorder[i] == val){
-            return i;
-        }
-    }
-    return -1; //it will not come because due to constrainsts
-  }
+//   int search(int val,vector<int>& inorder,int left,int right){
+//     for(int i = left;i<=right;i++)
+//     {
+//         if(inorder[i] == val){
+//             return i;
+//         }
+//     }
+//     return -1; //it will not come because due to constrainsts
+//   }
 
-   TreeNode* helper(vector<int>& preorder, vector<int>& inorder,int& idx,int left,int right){
+   TreeNode* helper(vector<int>& preorder, vector<int>& inorder,int& idx,int left,int right,unordered_map<int,int>& mp){
      if(left>right) return NULL;
 
       TreeNode* root = new TreeNode(preorder[idx]);
-      int inIdx = search(preorder[idx],inorder,left,right);
+      int inIdx = mp[preorder[idx]];
       idx++;
-      root->left = helper(preorder,inorder,idx,left,inIdx-1);
-      root->right = helper(preorder,inorder,idx,inIdx+1,right);
+      root->left = helper(preorder,inorder,idx,left,inIdx-1,mp);
+      root->right = helper(preorder,inorder,idx,inIdx+1,right,mp);
 
       return root;
    }
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
         int idx = 0;
-     return helper(preorder,inorder,idx,0,inorder.size()-1);
+        unordered_map<int,int>mp;
+        for(int i = 0;i<inorder.size();i++)
+        {
+            mp[inorder[i]] = i;
+        }
+     return helper(preorder,inorder,idx,0,inorder.size()-1,mp);
     }
 };
