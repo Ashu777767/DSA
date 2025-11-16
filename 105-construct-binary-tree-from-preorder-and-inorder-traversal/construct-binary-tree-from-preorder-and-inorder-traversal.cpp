195dd3ac89257f29178a60923c2ad745bf5103e1
ashutosh kumar jha <149995452+Ashu777767@users.noreply.github.com>
2025-11-16 11:07:56 +0530

    Time: 1 ms (84.19%) | Memory: 27.4 MB (52.74%) - LeetSync

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -11,34 +11,20 @@
  */
 class Solution {
 public:
-//   int search(int val,vector<int>& inorder,int left,int right){
-//     for(int i = left;i<=right;i++)
-//     {
-//         if(inorder[i] == val){
-//             return i;
-//         }
-//     }
-//     return -1; //it will not come because due to constrainsts
-//   }
-
-   TreeNode* helper(vector<int>& preorder, vector<int>& inorder,int& idx,int left,int right,unordered_map<int,int>& mp){
+     int idx = 0;
+     TreeNode* treebuild(vector<int>& preorder, vector<int>& inorder,int left,int right,unordered_map<int,int>& mp){
      if(left>right) return NULL;
-
-      TreeNode* root = new TreeNode(preorder[idx]);
-      int inIdx = mp[preorder[idx]];  //better t.c
-      idx++;
-      root->left = helper(preorder,inorder,idx,left,inIdx-1,mp);
-      root->right = helper(preorder,inorder,idx,inIdx+1,right,mp);
-
+     TreeNode* root = new TreeNode(preorder[idx]);
+     int  inidx = mp[preorder[idx++]];
+     root->left = treebuild(preorder,inorder,left,inidx-1,mp);
+     root->right = treebuild(preorder,inorder,inidx+1,right,mp);
       return root;
-   }
+     }
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-        int idx = 0;
-        unordered_map<int,int>mp;
-        for(int i = 0;i<inorder.size();i++)
-        {
-            mp[inorder[i]] = i;
-        }
-     return helper(preorder,inorder,idx,0,inorder.size()-1,mp);
+       unordered_map<int,int>mp;
+       for(int i = 0;i<inorder.size();i++){
+        mp[inorder[i]] = i;
+       }
+       return treebuild(preorder,inorder,0,inorder.size()-1,mp);
     }
 };
