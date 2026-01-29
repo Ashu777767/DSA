b79d2453fa1743fdbd5bc7fa4e1fa004c6ec1cf5
ashutosh kumar jha <149995452+Ashu777767@users.noreply.github.com>
2026-01-29 15:22:27 +0530

    Time: 50 ms (50.77%) | Memory: 28.9 MB (90.14%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -2,29 +2,31 @@ class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         sort(nums.begin(),nums.end());
-        int n = nums.size();
-        int left;
-        int right;
         vector<vector<int>>ans;
-        for(int i = 0;i<n-2;i++){
-            if (i > 0 && nums[i] == nums[i - 1]) continue;
-           left = i+1;
-           right = n-1;
-           while(left<right){
-            int sum = nums[i]+nums[left]+nums[right];
-            if(sum == 0){
-                ans.push_back({nums[i],nums[left],nums[right]});
-                while (left < right && nums[left] == nums[left + 1]) left++;
-                    while (left < right && nums[right] == nums[right - 1]) right--;
-                 left++;
+        for(int  i = 0;i<nums.size()-2;i++)
+        {
+            int left = i+1;
+            int right = nums.size()-1;
+            if(i>0 && nums[i] == nums[i-1]){
+                continue;
             }
-            else if(sum<0){ //negative then increment left
-                left++;
+            while(left<right){
+                int sum = nums[i]+nums[left]+nums[right];
+                if(sum == 0){
+                    ans.push_back({nums[i],nums[left],nums[right]});
+                
+                while(left<right && nums[left] == nums[left+1]) left++;
+                while(left<right && nums[right] == nums[right-1]) right--;
+                left++; // if one case is true then normally next pair is incresed by left to form next pair
+                }
+                else if(sum<0){
+                    left++;
+                }
+                else if(sum>0){
+                    right--;
+
+                }
             }
-            else{
-                right--;
-            }
-           }
         }
         return ans;
     }
