e9bd0c19bd400122efefd575456915626efe083c
ashutosh kumar jha <149995452+Ashu777767@users.noreply.github.com>
2026-02-21 17:45:54 +0530

    Time: 3 ms (91.82%) | Memory: 15 MB (85.66%) - LeetSync

--- 138-copy-list-with-random-pointer/copy-list-with-random-pointer.cpp
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.cpp
@@ -17,36 +17,28 @@ public:
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
-        if(head == NULL) return NULL;
-        //O(n) traversal;
-        //step1: cloning
         Node* temp = head;
-        while(temp!= NULL){
+        while(temp){
             Node* copy = new Node(temp->val);
-            copy->next = temp->next; //7->13
-            temp->next = copy;//7->7->13
-            temp = copy->next; // temp becomes 13
+            copy->next = temp->next;
+            temp->next = copy;
+            temp = copy->next;
         }
-        //
-        //step 2:copying the random link in clone nodes
         temp = head;
-        while(temp!=NULL){
-            if(temp->random != NULL){
-            temp->next->random = temp->random->next;
-            }//copy 13->random = original 13->7 here 7 is of original list so temp->random->next means 13->7->7' now its points to copy
-          temp = temp->next->next;
+        while(temp){
+            if(temp->random)
+            temp->next->random = temp->random->next; ///duplicatere se milega
+            temp = temp->next->next;
         }
-        //step 3: splitting the lists
-        Node* dummy = new Node(0); //without dummy we cant restore the original list
-        Node* copynode = dummy;
+        Node* dummy = new Node(0);
+        Node* copylist = dummy;
         temp = head;
-        while(temp != NULL){
-            copynode->next = temp->next;
-            temp->next = temp->next->next; //restore the original list
+        while(temp){
+            copylist->next = temp->next;
+            copylist = copylist->next;
+            temp->next = temp->next->next;
             temp = temp->next;
-            copynode = copynode->next;
         }
-
-          return dummy->next;   //returns the copy lists head;
+        return dummy->next;
     }
 };
