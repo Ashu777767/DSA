cfce129f014d2fb8bbf4668551685e2f83b1bf38
ashutosh kumar jha <149995452+Ashu777767@users.noreply.github.com>
2026-02-26 15:10:56 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (84.50%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        //dekh tu jo brute force socha woh saki toh hai lekin woh tle marega maybe kyu t.c sahi nhi hai
         unordered_map<int,int>freq;
-        for(auto n:nums){
-            freq[n]++;      //har number ka freq store hogya
+        for(int i = 0;i<nums.size();i++)
+        {
+            freq[nums[i]]++;
         }
-        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;   //min heap
-        for(auto &it:freq){
-            pq.push({it.second,it.first});   //k elements lega aur use store karega desecending order mai as per freq 
-            if(pq.size()>k) pq.pop(); //so that chotta freq remove hojayega
+        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
+        for( auto &it:freq){
+            pq.push({it.second,it.first});
+            if(pq.size()>k) pq.pop();
         }
-          vector<int>ans;
-        while(!pq.empty()){
-           ans.push_back(pq.top().second);
-           pq.pop();
-        }
-
-         return ans;
+        vector<int>ans;
+       while(!pq.empty()){
+        ans.push_back(pq.top().second);
+        pq.pop();
+       }
+       return ans;
     }
 };
