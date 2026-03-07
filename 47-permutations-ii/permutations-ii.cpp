7cd810b6e339cab5ec664d2f2491aeed970dcc1a
ashutosh kumar jha <149995452+Ashu777767@users.noreply.github.com>
2026-03-07 18:25:43 +0530

    Time: 0 ms (100.00%) | Memory: 11.4 MB (81.75%) - LeetSync

--- 47-permutations-ii/permutations-ii.cpp
+++ 47-permutations-ii/permutations-ii.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
-     void  permutations(vector<int>& nums,int idx, vector<vector<int>>& ans)
-  {
-    if(idx>=nums.size())
-    {
-        ans.push_back({nums});
+void permutations(vector<vector<int>>&ans,vector<int>&temp,vector<bool>& used,vector<int>& nums)
+{
+    if(nums.size() == temp.size()){
+        ans.push_back(temp);
         return ;
     }
-    unordered_map<int,int>mp;
-    for(int i = idx;i<nums.size();i++){
-          if(mp.find(nums[i])!=mp.end()){
-              continue;
-          }
-          mp[nums[i]]++;
-        swap(nums[i],nums[idx]);
-        permutations(nums,idx+1,ans);
-        swap(nums[i],nums[idx]);   //backtracking to get the previous array
+    for(int i = 0;i<nums.size();i++){
+        if(used[i] == true) continue;
+        if(i>0 && nums[i] == nums[i-1] && !used[i-1]) continue;
+        used[i] = true;
+        temp.push_back(nums[i]);
+        permutations(ans,temp,used,nums);
+        temp.pop_back();
+        used[i] = false;
     }
-  }
+}
     vector<vector<int>> permuteUnique(vector<int>& nums) {
         sort(nums.begin(),nums.end());
         vector<vector<int>>ans;
-        permutations(nums,0,ans);
+        vector<int>temp;
+        vector<bool>used(nums.size(), false);
+        permutations(ans,temp,used,nums);
         return ans;
     }
 };
