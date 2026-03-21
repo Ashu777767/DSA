c5bbe744a5f19d8f059779aa56496b4b5cc02c05
ashutosh kumar jha <149995452+Ashu777767@users.noreply.github.com>
2026-03-21 12:08:06 +0530

    Time: 4 ms (45.62%) | Memory: 17.5 MB (71.33%) - LeetSync

--- 692-top-k-frequent-words/top-k-frequent-words.cpp
+++ 692-top-k-frequent-words/top-k-frequent-words.cpp
@@ -1,18 +1,5 @@
 class Solution {
 public:
-    struct cmp {
-        bool operator()(pair<string,int>& a, pair<string,int>& b) {
-            // Case 1: same frequency
-            if(a.second == b.second) {
-                return a.first < b.first; 
-                // lexicographically smaller → lower priority (will go down)
-            }
-            // Case 2: different frequency
-            return a.second > b.second; 
-            // higher freq → lower priority (we want to keep it)
-        }
-    };
-
     vector<string> topKFrequent(vector<string>& words, int k) {
         unordered_map<string,int> freq;
 
@@ -21,29 +8,31 @@ public:
             freq[w]++;
         }
 
-        // Step 2: min heap
-        priority_queue<
-            pair<string,int>,
-            vector<pair<string,int>>,
-            cmp
-        > pq;
+        int n = words.size();
 
-        for(auto &it : freq) {
-            pq.push(it);
+        // Step 2: bucket (index = frequency)
+        vector<vector<string>> bucket(n + 1);
 
-            if(pq.size() > k) {
-                pq.pop();
+        for(auto &w : freq) {
+            bucket[w.second].push_back(w.first);
+        }
+
+        // Step 3: traverse from high freq
+        vector<string> ans;
+
+        for(int i = n; i >= 0 && ans.size() < k; i--) {
+            
+            if(!bucket[i].empty()) {
+                //  VERY IMPORTANT
+                sort(bucket[i].begin(), bucket[i].end());
+
+                for(string &word : bucket[i]) {
+                    ans.push_back(word);
+                    if(ans.size() == k) break;
+                }
             }
         }
 
-        // Step 3: extract answer
-        vector<string> ans;
-        while(!pq.empty()) {
-            ans.push_back(pq.top().first);
-            pq.pop();
-        }
-
-        reverse(ans.begin(), ans.end());
         return ans;
     }
 };
