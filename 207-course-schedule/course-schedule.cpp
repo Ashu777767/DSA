dbc3530303c0d6d89ceab7dbf065124e13a21caf
ashutosh kumar jha <149995452+Ashu777767@users.noreply.github.com>
2026-04-03 12:05:11 +0530

    Time: 56 ms (5.01%) | Memory: 17.7 MB (99.16%) - LeetSync

---
+++ 207-course-schedule/course-schedule.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+bool isCycleDFS(int src, vector<bool>& vis, vector<bool>& recPath, vector<vector<int>>& adj) {
+    
+    vis[src] = true;
+    recPath[src] = true;
+
+    for(int i =0;i<adj.size();i++) {
+          int v = adj[i][1];
+          int u = adj[i][0];
+        // Case 1: Not visited → go deeper
+        if(u == src){
+        if(!vis[v]) {
+            if(isCycleDFS(v, vis, recPath, adj)) {
+                return true;
+            }
+        }
+        
+
+        // Case 2: Already in recursion path → cycle
+        else if(recPath[v]) {
+            return true;
+        }
+        }
+    }
+
+    // Backtracking step
+    recPath[src] = false;
+
+    return false;
+}
+    bool canFinish(int n, vector<vector<int>>& edges) {
+        vector<bool>vis(n,false);
+        vector<bool>recPath(n,false);
+        for (int i = 0; i < n; i++) {
+            if (!vis[i]) {
+                if (isCycleDFS(i, vis, recPath, edges)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
