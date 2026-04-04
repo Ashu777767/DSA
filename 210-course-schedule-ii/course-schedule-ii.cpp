3a46d61ec72843f271ba8dede1c4d9d5c8b6a521
ashutosh kumar jha <149995452+Ashu777767@users.noreply.github.com>
2026-04-04 12:13:10 +0530

    Time: 0 ms (100.00%) | Memory: 18.4 MB (57.30%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.cpp
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -1,42 +1,54 @@
 class Solution {
 public:
-bool isCycleDFS(int src,int n,vector<vector<int>> &prerequisites,vector<bool>& vis,vector<bool>&recPath,stack<int>& st){
+
+bool isCycleDFS(int src, vector<vector<int>>& adj,
+                vector<bool>& vis, vector<bool>& recPath,
+                stack<int>& st) {
+
     vis[src] = true;
     recPath[src] = true;
-    for(int i = 0;i<prerequisites.size();i++){
-        int v = prerequisites[i][0];
-        int u = prerequisites[i][1];
-        if(u == src){
-            if(!vis[v]){
-            if(isCycleDFS(v,n,prerequisites,vis,recPath,st)) return true;
-            }
-            else if(recPath[v]){
-                  return true;
-            }
+
+    for(auto v : adj[src]) {
+        if(!vis[v]) {
+            if(isCycleDFS(v, adj, vis, recPath, st)) return true;
+        }
+        else if(recPath[v]) {
+            return true;
         }
     }
+
     recPath[src] = false;
     st.push(src);
     return false;
-
 }
-    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<bool>vis(numCourses,false);
-        vector<bool>recPath(numCourses,false);
-        stack<int>st;
-        for(int i = 0;i<numCourses;i++){
-           if(!vis[i]){
-            if(isCycleDFS(i,numCourses,prerequisites,vis,recPath,st)){
+
+vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+
+    vector<vector<int>> adj(numCourses);
+
+    // build graph
+    for(auto &p : prerequisites) {
+        adj[p[1]].push_back(p[0]);
+    }
+
+    vector<bool> vis(numCourses, false);
+    vector<bool> recPath(numCourses, false);
+    stack<int> st;
+
+    for(int i = 0; i < numCourses; i++) {
+        if(!vis[i]) {
+            if(isCycleDFS(i, adj, vis, recPath, st)) {
                 return {};
             }
-           }
         }
-        vector<int>ans;
-        while(!st.empty()){
-            ans.push_back(st.top());
-            st.pop();
-        }
-        return ans;
-
     }
+
+    vector<int> ans;
+    while(!st.empty()) {
+        ans.push_back(st.top());
+        st.pop();
+    }
+
+    return ans;
+}
 };
