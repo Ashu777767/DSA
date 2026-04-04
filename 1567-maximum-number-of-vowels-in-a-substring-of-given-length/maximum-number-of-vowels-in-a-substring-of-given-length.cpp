c40a2f534c8c615007bc7ed44f71cea92210c8df
ashutosh kumar jha <149995452+Ashu777767@users.noreply.github.com>
2026-04-04 14:53:54 +0530

    Time: 0 ms (100.00%) | Memory: 13.2 MB (59.51%) - LeetSync

--- 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.cpp
+++ 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
-bool isVowel(char ch){
-    return (ch == 'a'||ch == 'e' || ch == 'i' || ch == 'o'|| ch == 'u');
-}
+    bool isVowel(char ch) {
+        return (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u');
+    }
     int maxVowels(string s, int k) {
         int n = s.size();
         int left = 0;
         int count = 0;
         int maximum = 0;
-        for(int i = 0;i<n;i++){
-            count = isVowel(s[i])?count+1:count;
-            while((i-left+1) == k){
-                maximum = max(count,maximum);
-                if(isVowel(s[left]))
-                count--;
+        for (int i = 0; i < n; i++) {
+            count = isVowel(s[i]) ? count + 1 : count;
+            if ((i - left + 1) == k) {
+                maximum = max(count, maximum);
+                if (isVowel(s[left]))
+                    count--;
                 left++;
             }
         }
