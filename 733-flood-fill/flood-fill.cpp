7a2c88e8230e809a390e8fc0c5b2d3bd0eef278e
ashutosh kumar jha <149995452+Ashu777767@users.noreply.github.com>
2026-04-09 15:48:56 +0530

    Time: 0 ms (100.00%) | Memory: 18 MB (89.64%) - LeetSync

--- 733-flood-fill/flood-fill.cpp
+++ 733-flood-fill/flood-fill.cpp
@@ -1,24 +1,33 @@
 class Solution {
 public:
- void floodthebox(int sr, int sc,vector<vector<int>>& image, vector<vector<bool>>& vis,int color,int rows,int col,int original){
-    if(sr<0 || sc<0 || sr>rows || sc>col || vis[sr][sc] || image[sr][sc] != original){
-        return ;
+    void floodthebox(int sr, int sc, vector<vector<int>>& image, int color,
+                     int rows, int col, int original) {
+        if (sr < 0 || sc < 0 || sr > rows || sc > col ||
+            image[sr][sc] != original) {
+            return;
+        }
+        // vis[sr][sc] = true; not required beacuse everytime we visit a correct
+        // node we mark it to color so why wee need vis the image[sr][sc] itself
+        // handels it no need
+        image[sr][sc] = color;
+        floodthebox(sr - 1, sc, image, color, rows, col, original); // up
+        floodthebox(sr + 1, sc, image, color, rows, col, original); // down
+        floodthebox(sr, sc - 1, image, color, rows, col, original); // left
+        floodthebox(sr, sc + 1, image, color, rows, col, original); // right
+        return;
     }
-    vis[sr][sc] = true;
-    image[sr][sc] = color;
-    floodthebox(sr-1,sc,image,vis,color,rows,col,original);  //up  
-    floodthebox(sr+1,sc,image,vis,color,rows,col,original);    //down
-    floodthebox(sr,sc-1,image,vis,color,rows,col,original);    //left
-    floodthebox(sr,sc+1,image,vis,color,rows,col,original);    //right
-    return ;
- }
 
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        int original =  image[sr][sc];
-        int rows = image.size();
-        int col = image[0].size();
-        vector<vector<bool>> vis(rows, vector<bool>(col, false));
-      floodthebox(sr,sc,image,vis,color,rows-1,col-1,original); 
-      return image;
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc,
+                                  int color) {
+        if (image[sr][sc] == color)
+            return image; // if vis removed this edge case came what if
+                          // everthing is 0 then it may cause overflow so add
+                          // this edgecase check condition
+        int original = image[sr][sc];
+        int rows = image.size() - 1;
+        int col = image[0].size() - 1;
+
+        floodthebox(sr, sc, image, color, rows, col, original);
+        return image;
     }
 };
