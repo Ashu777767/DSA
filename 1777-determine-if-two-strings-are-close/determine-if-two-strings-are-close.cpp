ca3dece186b44c70a3098b8a0102604971c0f17a
ashutosh kumar jha <149995452+Ashu777767@users.noreply.github.com>
2026-04-10 11:48:32 +0530

    Time: 5 ms (77.98%) | Memory: 23.5 MB (65.14%) - LeetSync

--- 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.cpp
+++ 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.cpp
@@ -5,21 +5,27 @@ public:
 
         vector<int> freq1(26, 0), freq2(26, 0);
 
-        // Count frequencies
         for(char ch : word1) freq1[ch - 'a']++;
         for(char ch : word2) freq2[ch - 'a']++;
 
-        // ✅ Condition 1: Same character presence
+        // ✅ Condition 1: same characters
         for(int i = 0; i < 26; i++){
             if((freq1[i] == 0 && freq2[i] != 0) ||
                (freq1[i] != 0 && freq2[i] == 0))
                 return false;
         }
 
-        // ✅ Condition 2: Same frequency distribution
-        sort(freq1.begin(), freq1.end());
-        sort(freq2.begin(), freq2.end());
+        // ✅ Count frequency of frequencies
+        unordered_map<int,int> count1, count2;
 
-        return freq1 == freq2;
+        for(int f : freq1){
+            if(f > 0) count1[f]++;
+        }
+
+        for(int f : freq2){
+            if(f > 0) count2[f]++;
+        }
+
+        return count1 == count2;
     }
 };
