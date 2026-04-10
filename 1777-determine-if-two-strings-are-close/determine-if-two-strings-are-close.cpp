c0628d94f07acbdb8e70feb37db2be6f626c37f8
ashutosh kumar jha <149995452+Ashu777767@users.noreply.github.com>
2026-04-10 11:42:26 +0530

    Time: 25 ms (37.99%) | Memory: 23.7 MB (30.77%) - LeetSync

--- 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.cpp
+++ 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.cpp
@@ -1,29 +1,30 @@
 class Solution {
 public:
     bool closeStrings(string word1, string word2) {
-        if(word1.size()!=word2.size()) return false;
-        unordered_map<char,int>mp;
-        unordered_map<char,int>mp1;
-        for(auto ch:word2){
+        if (word1.size() != word2.size())
+            return false;
+        unordered_map<char, int> mp;
+        unordered_map<char, int> mp1;
+        for (auto ch : word2) {
             mp1[ch]++;
         }
-         for(auto ch:word1){
+        for (auto ch : word1) {
             mp[ch]++;
         }
-      for(auto [ch,f]:mp){
-        if(mp1.find(ch) == mp1.end()) return false;
-      }
-       vector<int> v1, v2;
+        for (auto [ch, f] : mp) {
+            if (mp1.find(ch) == mp1.end())
+                return false;
+        }
+        vector<int> v1, v2;
 
-        for(auto [ch, freq] : mp) v1.push_back(freq);
-        for(auto [ch, freq] : mp1) v2.push_back(freq);
+        for (auto [ch, freq] : mp)
+            v1.push_back(freq);
+        for (auto [ch, freq] : mp1)
+            v2.push_back(freq);
 
         sort(v1.begin(), v1.end());
         sort(v2.begin(), v2.end());
 
-       
-        
-        return v1==v2;
-
+        return v1 == v2;
     }
 };
