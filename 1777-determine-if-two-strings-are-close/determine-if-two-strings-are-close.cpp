473451ed0d9d1a27baf1ddcd50b1d91c4b64b032
ashutosh kumar jha <149995452+Ashu777767@users.noreply.github.com>
2026-04-10 11:49:33 +0530

    Time: 6 ms (75.17%) | Memory: 23.4 MB (82.89%) - LeetSync

--- 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.cpp
+++ 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.cpp
@@ -1,31 +1,27 @@
 class Solution {
 public:
     bool closeStrings(string word1, string word2) {
-        if(word1.size() != word2.size()) return false;
-
-        vector<int> freq1(26, 0), freq2(26, 0);
-
-        for(char ch : word1) freq1[ch - 'a']++;
-        for(char ch : word2) freq2[ch - 'a']++;
-
-        // ✅ Condition 1: same characters
-        for(int i = 0; i < 26; i++){
-            if((freq1[i] == 0 && freq2[i] != 0) ||
-               (freq1[i] != 0 && freq2[i] == 0))
+        if(word1.size() != word2.size()){
+            return false;
+        }
+        int freq1[26] = {0};
+        int freq2[26] = {0};
+        for( int i = 0;i<word1.size();i++){
+            freq1[word1[i] - 'a']++;
+            freq2[word2[i] - 'a']++;
+        }
+        for(int i=0;i<26;i++){
+            if((freq1[i] != 0 && freq2[i] == 0) || (freq2[i] != 0 && freq1[i] == 0) ){
                 return false;
+            }
         }
-
-        // ✅ Count frequency of frequencies
-        unordered_map<int,int> count1, count2;
-
-        for(int f : freq1){
-            if(f > 0) count1[f]++;
+        sort(freq1,freq1 + 26);
+        sort(freq2,freq2 + 26);
+        for(int i = 0;i < 26;i++){
+            if(freq1[i] != freq2[i]){
+                return false;
+            }
         }
-
-        for(int f : freq2){
-            if(f > 0) count2[f]++;
-        }
-
-        return count1 == count2;
+        return true;
     }
 };
