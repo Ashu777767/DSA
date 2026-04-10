1cd05bd334cffac30dd20cb2ad0a4fe682ada1f3
ashutosh kumar jha <149995452+Ashu777767@users.noreply.github.com>
2026-04-10 12:25:52 +0530

    Time: 7 ms (88.27%) | Memory: 39 MB (32.26%) - LeetSync

--- 2428-equal-row-and-column-pairs/equal-row-and-column-pairs.cpp
+++ 2428-equal-row-and-column-pairs/equal-row-and-column-pairs.cpp
@@ -1,28 +1,38 @@
+#include <vector>
+#include <map>
+
+using namespace std;
+
+static const int _ = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    return 0;
+}();
+
+
 class Solution {
 public:
     int equalPairs(vector<vector<int>>& grid) {
         int n = grid.size();
 
-        unordered_map<string, int> mp;
+        map<vector<int>, int> mp;
 
         // store rows
         for (auto row : grid) {
-            string s = "";
-            for (int x : row) {
-                s += to_string(x) + "#";
-            }
-            mp[s]++;
+            mp[row]++;
         }
 
         int count = 0;
 
         // check columns
         for (int j = 0; j < n; j++) {
-            string s = "";
+            vector<int> col;
+
             for (int i = 0; i < n; i++) {
-                s += to_string(grid[i][j]) + "#";
+                col.push_back(grid[i][j]);
             }
-            count += mp[s];
+
+            count += mp[col];
         }
 
         return count;
