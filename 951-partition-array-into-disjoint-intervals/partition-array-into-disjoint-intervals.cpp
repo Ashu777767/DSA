32f055eb532ac0c1a37f58f34e4e74888c309225
ashutosh kumar jha <149995452+Ashu777767@users.noreply.github.com>
2026-04-18 12:49:38 +0530

    Time: 0 ms (100.00%) | Memory: 98 MB (92.05%) - LeetSync

---
+++ 951-partition-array-into-disjoint-intervals/partition-array-into-disjoint-intervals.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int partitionDisjoint(vector<int>& nums) {
+        int n = nums.size();
+        int leftMax = nums[0];
+        int maxSeen = nums[0];
+        int pos = 0;
+
+        for (int i = 1; i < n; i++) {
+            if (nums[i] < leftMax) {
+                // Must include i in left → extend
+                pos = i;
+                leftMax = maxSeen; // update leftMax to current maxSeen
+            } 
+            else maxSeen = max(maxSeen, nums[i]);  
+        }
+
+        return pos + 1;
+    }
+};
