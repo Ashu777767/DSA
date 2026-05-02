e1ca4fb646e8d3ba8dbc5500e7010f665a74e9bb
ashutosh kumar jha <149995452+Ashu777767@users.noreply.github.com>
2026-05-02 15:45:05 +0530

    Time: 8 ms (59.15%) | Memory: 35.7 MB (62.14%) - LeetSync

--- 2038-nearest-exit-from-entrance-in-maze/nearest-exit-from-entrance-in-maze.cpp
+++ 2038-nearest-exit-from-entrance-in-maze/nearest-exit-from-entrance-in-maze.cpp
@@ -1,32 +1,34 @@
 class Solution {
 public:
     int nearestExit(vector<vector<char>>& maze, vector<int>& entrance) {
-        queue<pair<pair<int,int>,int>> q;
+        queue<pair<pair<int, int>, int>> q;
         q.push({{entrance[0], entrance[1]}, 0});
 
         int rsize = maze.size();
         int csize = maze[0].size();
 
-        vector<pair<int,int>> direction{{-1,0},{1,0},{0,-1},{0,1}};
+        vector<pair<int, int>> direction{{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
 
         // mark entrance visited
         maze[entrance[0]][entrance[1]] = '+';
 
-        while(!q.empty()){
+        while (!q.empty()) {
             int r = q.front().first.first;
             int c = q.front().first.second;
             int moves = q.front().second;
             q.pop();
 
-            for(auto [rd,cd] : direction){
+            for (auto [rd, cd] : direction) {
                 int newrow = r + rd;
                 int newcol = c + cd;
 
-                if(newrow < 0 || newcol < 0 || newrow >= rsize || newcol >= csize || maze[newrow][newcol] == '+')
+                if (newrow < 0 || newcol < 0 || newrow >= rsize ||
+                    newcol >= csize || maze[newrow][newcol] == '+')
                     continue;
 
                 // exit check
-                if(newrow == 0 || newcol == 0 || newrow == rsize-1 || newcol == csize-1)
+                if (newrow == 0 || newcol == 0 || newrow == rsize - 1 ||
+                    newcol == csize - 1)
                     return moves + 1;
 
                 // ✅ mark BEFORE pushing
