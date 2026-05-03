ed4d8838593d58089e14d84b26e48a5988f31128
ashutosh kumar jha <149995452+Ashu777767@users.noreply.github.com>
2026-05-03 18:11:21 +0530

    Time: 66 ms (5.01%) | Memory: 17.5 MB (99.88%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,37 +1,38 @@
 class Solution {
 public:
-bool isCycleDFS(int src, vector<bool>& vis, vector<bool>& recPath, vector<vector<int>>& adj) {
-    
-    vis[src] = true;
-    recPath[src] = true;
+    bool isCycleDFS(int src, vector<bool>& vis, vector<bool>& recPath,
+                    vector<vector<int>>& adj) {
 
-    for(int i =0;i<adj.size();i++) {
-          int v = adj[i][0];//put any value cuz in main function we are checking from every staring point
-          int u = adj[i][1];
-        // Case 1: Not visited → go deeper
-        if(u == src){
-        if(!vis[v]) {
-            if(isCycleDFS(v, vis, recPath, adj)) {
-                return true;
+        vis[src] = true;
+        recPath[src] = true;
+
+        for (int i = 0; i < adj.size(); i++) {
+            int v = adj[i][0]; // put any value cuz in main function we are
+                               // checking from every staring point
+            int u = adj[i][1];
+            // Case 1: Not visited → go deeper
+            if (u == src) {
+                if (!vis[v]) {
+                    if (isCycleDFS(v, vis, recPath, adj)) {
+                        return true;
+                    }
+                }
+
+                // Case 2: Already in recursion path → cycle
+                else if (recPath[v]) {
+                    return true;
+                }
             }
         }
-        
 
-        // Case 2: Already in recursion path → cycle
-        else if(recPath[v]) {
-            return true;
-        }
-        }
+        // Backtracking step
+        recPath[src] = false;
+
+        return false;
     }
-
-    // Backtracking step
-    recPath[src] = false;
-
-    return false;
-}
     bool canFinish(int n, vector<vector<int>>& edges) {
-        vector<bool>vis(n,false);
-        vector<bool>recPath(n,false);
+        vector<bool> vis(n, false);
+        vector<bool> recPath(n, false);
         for (int i = 0; i < n; i++) {
             if (!vis[i]) {
                 if (isCycleDFS(i, vis, recPath, edges)) {
