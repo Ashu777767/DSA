2a72b5957c602b199d70442ff8e1cfd8efd345d2
ashutosh kumar jha <149995452+Ashu777767@users.noreply.github.com>
2026-05-03 11:17:46 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (36.13%) - LeetSync

--- 4297-sum-of-primes-between-number-and-its-reverse/sum-of-primes-between-number-and-its-reverse.cpp
+++ 4297-sum-of-primes-between-number-and-its-reverse/sum-of-primes-between-number-and-its-reverse.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
- bool isprime(int n){
-     if(n == 0 || n == 1){
-         return false;
-     }
-     if(n==2) return true;
-        for(int i = 2;i<=n/2;i++){
-            if(n%i == 0){
-                return false;
+void  findprimes( vector<bool>& isPrime,int r){
+    isPrime[0] = false;
+    isPrime[1] = false;
+    for(int i = 2;i*i<=r;i++){
+        if(isPrime[i]){
+            for(int j = i*i;j<=r;j+=i){
+                isPrime[j] = false;
             }
         }
-     return true;
     }
-    
+    return ;
+}
+
     int sumOfPrimesInRange(int n) {
          int reverse = n;
         int r = 0;
@@ -20,13 +20,14 @@ public:
             r = 10*r+(reverse%10);
             reverse/=10;
         }
-        long long ans = 0;
         int start = min(n,r);
         int end = max(n,r);
-        for(int i = start;i<=end;i++){
-            if(isprime(i)){
-                ans+=i;
-            }
+        vector<bool>isPrime(end+1,true);
+        findprimes(isPrime,end);
+        long long ans = 0;
+        for(int i =start;i<=end;i++){
+            if(isPrime[i]) ans+=i;
+            
         }
         return ans;
     }
