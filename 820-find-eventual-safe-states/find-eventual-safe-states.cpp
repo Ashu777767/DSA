5ec07ce975b433e9ba84d65492f3419fc78999fe
ashutosh kumar jha <149995452+Ashu777767@users.noreply.github.com>
2026-05-16 18:26:08 +0530

    Time: 55 ms (26.80%) | Memory: 66.4 MB (20.57%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.cpp
+++ 820-find-eventual-safe-states/find-eventual-safe-states.cpp
@@ -1,45 +1,58 @@
 class Solution {
 public:
-bool safeNode(int src,vector<bool> &vis,vector<bool>&isSafe,vector<vector<int>>& graph,bool flag){
-    vis[src] = true;
-    for(auto node:graph[src]){
-        if(!vis[node] && !isSafe[node]){
-            if(safeNode(node,vis,isSafe,graph,true)){
-                isSafe[node] = true;
-            }else{
-                flag = false; //if one adjcent node found non safe the the vertex is not valid
-            }
-        }else if(!isSafe[node]){ //if one adjcent node is vis and not safe means not valid vertex for answer
-            flag = false;
-
-        }
-    }
-    return flag;
-}
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+        
         int n = graph.size();
-        vector<bool>isSafe(n,false);
-        vector<bool>vis(n,false);
-        for(int i = 0;i<n;i++){  //marking terminal nodes at first
-            if(graph[i].size() == 0){
-                isSafe[i] = true;
-                vis[i] = true;
+
+        // reverse graph
+        vector<vector<int>> revGraph(n);
+
+        // outdegree count
+        vector<int> outDegree(n, 0);
+
+        // Build reverse graph
+        for(int i = 0; i < n; i++) {
+
+            outDegree[i] = graph[i].size();
+
+            for(auto node : graph[i]) {
+                revGraph[node].push_back(i);
             }
         }
-        for(int i = 0;i<n;i++){
-            if(!vis[i]){
-                if(safeNode(i,vis,isSafe,graph,true)){
-                    isSafe[i] = true;
+
+        queue<int> q;
+
+        // terminal nodes -> outdegree = 0
+        for(int i = 0; i < n; i++) {
+            if(outDegree[i] == 0) {
+                q.push(i);
+            }
+        }
+
+        vector<int> safeNodes;
+
+        // Kahn's Algorithm
+        while(!q.empty()) {
+
+            int node = q.front();
+            q.pop();
+
+            safeNodes.push_back(node);
+
+            // visit parents
+            for(auto parent : revGraph[node]) {
+
+                outDegree[parent]--;
+
+                if(outDegree[parent] == 0) {
+                    q.push(parent);
                 }
             }
         }
-        vector<int>ans;
-        for(int  i = 0;i<n;i++){
-            if(isSafe[i]){
-                ans.push_back(i);
-            }
-        }
-        return ans;
 
+        // answer should be sorted
+        sort(safeNodes.begin(), safeNodes.end());
+
+        return safeNodes;
     }
 };
