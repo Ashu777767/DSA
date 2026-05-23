aa00e00146b9bafe44c1ddbf8977e0ca3561e1c7
ashutosh kumar jha <149995452+Ashu777767@users.noreply.github.com>
2026-05-23 22:20:04 +0530

    Time: 85 ms (51.13%) | Memory: 44.5 MB (44.13%) - LeetSync

--- 744-network-delay-time/network-delay-time.cpp
+++ 744-network-delay-time/network-delay-time.cpp
@@ -4,31 +4,34 @@ public:
         // transforming it to the organized vertices and edges
         vector<vector<pair<int, int>>> adj(n + 1);
         for (auto& e : times) {
-            adj[e[0]].push_back({e[1], e[2]});
+            adj[e[0]].push_back(
+                {e[1], e[2]}); // transforming to proper graphs arrangement
         }
         priority_queue<pair<int, int>, vector<pair<int, int>>,
-                       greater<pair<int, int>>>
-            pq; // min heap
-        vector<int> dist(n + 1, INT_MAX);
+                       greater<pair<int, int>>> // to pick shortest distance
+                                                // edge first so pq
+                                                    pq; // min heap
+        vector<int> dist(n + 1, INT_MAX);               // intial dist
         vector<int> vis(n + 1, 0);
         pq.push({0, k}); // dist,node
-        //edge initalize source to 0 dist
+        // edge initalize source to 0 dist
         dist[k] = 0;
         while (!pq.empty()) {
             int u = pq.top().second;
             int dis = pq.top().first;
             pq.pop();
-            if (vis[u] == 1)
+            if (vis[u] == 1) // it is directed graph but still same node can
+                             // come twice so to prevent duplicate
                 continue;
             for (auto& Edge : adj[u]) {
                 int node = Edge.first;
                 int d = Edge.second;
-                if (dist[node] > dis + d) {
+                if (dist[node] > dis + d) { // edge relaxation step
                     dist[node] = dis + d;
                     pq.push({dist[node], node});
                 }
             }
-            vis[u] = 1;
+            vis[u] = 1; // vis
         }
         int ans = 0;
         for (int i = 1; i <= n; i++) {
