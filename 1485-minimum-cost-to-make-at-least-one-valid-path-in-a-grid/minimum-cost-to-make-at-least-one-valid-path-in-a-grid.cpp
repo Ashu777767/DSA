6802e97ddab5f92b85a0d529778214603da52476
ashutosh kumar jha <149995452+Ashu777767@users.noreply.github.com>
2026-05-30 16:58:43 +0530

    Time: 31 ms (36.55%) | Memory: 31.9 MB (15.27%) - LeetSync

--- 1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/minimum-cost-to-make-at-least-one-valid-path-in-a-grid.cpp
+++ 1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/minimum-cost-to-make-at-least-one-valid-path-in-a-grid.cpp
@@ -1,61 +1,83 @@
 class Solution {
 public:
- 
     int minCost(vector<vector<int>>& grid) {
         int r = grid.size();
         int c = grid[0].size();
-        priority_queue<pair<int, pair<int, int>>,
-                       vector<pair<int, pair<int, int>>>,
-                       greater<pair<int, pair<int, int>>>>
-            pq;
+
+        deque<pair<int, pair<int, int>>> dq;
+
         vector<vector<bool>> vis(r, vector<bool>(c, 0));
-        pq.push({0, {0, 0}});//cost,row,col
+
+        dq.push_front({0, {0, 0}}); // cost,row,col
+
         vector<pair<int, int>> direction = {
             {-1, 0}, // Up
             {0, 1},  // Right
             {1, 0},  // Down
             {0, -1}  // Left
         };
-        
-        while(!pq.empty()){
-           int cost = pq.top().first;
-           int row = pq.top().second.first;
-           int col = pq.top().second.second;
-           pq.pop();
-           if(vis[row][col]) continue;
-           vis[row][col] = true;
-           if(row == r-1 && col == c-1){
-            return cost;
-           }
-           bool flag = false;
-           for(auto &dir:direction){
-            int nrow = row+dir.first;
-            int ncol = col+dir.second;
-            if(nrow<0 || ncol<0 || nrow>=r || ncol>=c) continue;
-            int pathdir = grid[row][col];
-            if(!flag && pathdir == 1  && (row == nrow && col+1 == ncol)){
-                 pq.push({cost,{row,col+1}});
-                 flag = true;
-                 continue;
-            }else if(!flag && pathdir == 2  && (row == nrow && col-1 == ncol)){
-                 pq.push({cost,{row,col-1}});
-                 flag = true;
-                 continue;
-            }else if(!flag && pathdir == 3 && (row+1 == nrow && col == ncol)){
-                 pq.push({cost,{row+1,col}});
-                 flag = true;
-                 continue;
-            }else if(!flag && pathdir == 4  && (row-1 == nrow && col == ncol)){
-                 pq.push({cost,{row-1,col}});
-                 flag = true;
-                 continue;
-            }else{
-                pq.push({cost+1,{nrow,ncol}});
-            }
-            
 
-           }
+        while (!dq.empty()) {
+            int cost = dq.front().first;
+            int row = dq.front().second.first;
+            int col = dq.front().second.second;
+            dq.pop_front();
+
+            if (vis[row][col])
+                continue;
+
+            vis[row][col] = true;
+
+            if (row == r - 1 && col == c - 1) {
+                return cost;
+            }
+
+            bool flag = false;
+
+            for (auto& dir : direction) {
+                int nrow = row + dir.first;
+                int ncol = col + dir.second;
+
+                if (nrow < 0 || ncol < 0 || nrow >= r || ncol >= c)
+                    continue;
+
+                int pathdir = grid[row][col];
+
+                if (!flag && pathdir == 1 &&
+                    (row == nrow && col + 1 == ncol)) {
+
+                    dq.push_front({cost, {nrow, ncol}});
+                    flag = true;
+                    continue;
+
+                } else if (!flag && pathdir == 2 &&
+                           (row == nrow && col - 1 == ncol)) {
+
+                    dq.push_front({cost, {nrow, ncol}});
+                    flag = true;
+                    continue;
+
+                } else if (!flag && pathdir == 3 &&
+                           (row + 1 == nrow && col == ncol)) {
+
+                    dq.push_front({cost, {nrow, ncol}});
+                    flag = true;
+                    continue;
+
+                } else if (!flag && pathdir == 4 &&
+                           (row - 1 == nrow && col == ncol)) {
+
+                    dq.push_front({cost, {nrow, ncol}});
+                    flag = true;
+                    continue;
+
+                } else {
+
+                    dq.push_back({cost + 1, {nrow, ncol}});
+                }
+            }
         }
+
         return 0;
     }
 };
