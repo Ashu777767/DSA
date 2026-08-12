6137374860b5481d269171f8459c0e4a79629118
ashutosh kumar jha <149995452+Ashu777767@users.noreply.github.com>
2026-08-12 12:14:02 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (13.25%) - LeetSync

--- 216-combination-sum-iii/combination-sum-iii.cpp
+++ 216-combination-sum-iii/combination-sum-iii.cpp
@@ -1,50 +1,27 @@
 class Solution {
 public:
+void findcombination(vector<vector<int>>& ans,int k,int n,int sum,vector<int>temp,int index){
+    if(temp.size() == k &&sum == n){
+        ans.push_back(temp);
+        return ;
+    }
+     if(temp.size() >= k || sum >= n || index>9){
+        return ;
+    }
+    for(int i =index;i<=9;i++){
+        sum+=i;
+        temp.push_back(i);
+        findcombination(ans,k,n,sum,temp,i+1);
+        sum-=i;
+        temp.pop_back();
+    }
+
+
+}
     vector<vector<int>> combinationSum3(int k, int n) {
-
-        vector<vector<int>> ans;
-
-        for (int a = 1; a <= 9; a++) {
-            for (int b = a + 1; b <= 9; b++) {
-                for (int c = b + 1; c <= 9; c++) {
-                    for (int d = c + 1; d <= 9; d++) {
-                        for (int e = d + 1; e <= 9; e++) {
-                            for (int f = e + 1; f <= 9; f++) {
-                                for (int g = f + 1; g <= 9; g++) {
-                                    for (int h = g + 1; h <= 9; h++) {
-                                        for (int i = h + 1; i <= 9; i++) {
-
-                                            vector<int> temp = {
-                                                a,b,c,d,e,f,g,h,i
-                                            };
-
-                                            for (int j = 0; j < (1 << 9); j++) {
-
-                                                vector<int> current;
-                                                int sum = 0;
-
-                                                for (int x = 0; x < 9; x++) {
-                                                    if (j & (1 << x)) {
-                                                        current.push_back(temp[x]);
-                                                        sum += temp[x];
-                                                    }
-                                                }
-
-                                                if (current.size() == k &&
-                                                    sum == n) {
-                                                    ans.push_back(current);
-                                                }
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-
+        vector<vector<int>>ans;
+        vector<int>temp;
+        findcombination(ans,k,n,0,temp,1);
         return ans;
     }
 };
